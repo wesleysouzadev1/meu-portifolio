ec45d4d5b9c4dd9f1f3f2af990ef9ae618f4bb55
Wesley Souza <wesleysouzadev@hotmail.com>
2025-03-11 19:47:16 -0300

    menu do top

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
 import { MenuService } from '../services/menu.service';
 import { Menu } from '../models/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
 
-  constructor(private menuService: MenuService) {}
 
-    menuItems?: Array<Menu>;
+  constructor(private menuService: MenuService) { }
+  
+  menuStatus: string = 'closed';
+  
+  menuItems?: Array<Menu>;
 
-    ngOnInit() {
-      const menu = this.menuService.getItems();
+  ngOnInit() {
+    const menu = this.menuService.getItems();
 
-      this.menuItems = menu;
-    }
+    this.menuItems = menu;
+  }
+
+  menuClick() {
+
+    this.menuStatus = (this.menuStatus === 'open' ? 'closed' : 'open');
+  }
 
 }
