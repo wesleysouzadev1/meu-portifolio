9d721631a66678658807f1cb2a3d4c12e7ed029c
Wesley Souza <wesleysouzadev@hotmail.com>
2025-03-16 21:39:56 -0300

    box footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProfileService } from '../services/profile.service';
+import { Profile } from '../models/profile';
 
 @Component({
   selector: 'app-footer',
@@ -9,7 +10,15 @@ import { ProfileService } from '../services/profile.service';
 })
 export class FooterComponent {
 
-  constructor(private profileService: ProfileService) {
-    
+  constructor(private profileService: ProfileService) { }
+
+  profile?: Profile;
+  
+  date: Date = new Date();
+
+  ngOnInit () {
+
+    this.profile = this.profileService.getProfile();
   }
+
 }
